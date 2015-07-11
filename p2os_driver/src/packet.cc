9cec443f55d95b415a083de17098d1ea48f33317
Hunter Allen <allen286@purdue.edu>
2015-07-11 03:14:46 -0400

    Doing some cleanup of the code.

--- p2os_driver/src/packet.cc
+++ p2os_driver/src/packet.cc
@@ -1,7 +1,7 @@
 /*
  *  P2OS for ROS
  *  Copyright (C) 2000
- *     David Feil-Seifer, Brian Gerkey, Kasper Stoy, 
+ *      Hunter Allen, David Feil-Seifer, Brian Gerkey, Kasper Stoy,
  *      Richard Vaughan, & Andrew Howard
  *
  *
@@ -28,150 +28,148 @@
 #include <unistd.h>
 #include <stdlib.h> /* for exit() */
 #include <ros/ros.h>
-void P2OSPacket::Print() {
-  if (packet) {
-    ROS_INFO("\"");
-    for(int i=0;i<size;i++) {
-      ROS_INFO("%u ", packet[i]);
+
+void P2OSPacket::Print()
+{
+    if (packet) {
+        ROS_INFO("\"");
+        for(int i=0;i<size;i++)
+            ROS_INFO("%u ", packet[i]);
+        ROS_INFO("\"");
     }
-    ROS_INFO("\"");
-  }
 }
 
-void P2OSPacket::PrintHex() {
-  if (packet) {
-    ROS_INFO("\"");
-    for(int i=0;i<size;i++) {
-      ROS_INFO("0x%.2x ", packet[i]);
+void P2OSPacket::PrintHex()
+{
+    if (packet) {
+        ROS_INFO("\"");
+        for(int i=0;i<size;i++)
+            ROS_INFO("0x%.2x ", packet[i]);
+        ROS_INFO("\"");
     }
-    ROS_INFO("\"");
-  }
 }
 
 
 bool P2OSPacket::Check() {
-  short chksum;
-  chksum = CalcChkSum();
+    short chksum;
+    chksum = CalcChkSum();
 
-  if ( (chksum == packet[size-2] << 8) | packet[size-1])
-    return(true);
+    if ((chksum == packet[size-2] << 8) | packet[size-1])
+        return(true);
 
 
-  return(false);
+    return(false);
 }
 
 int P2OSPacket::CalcChkSum() {
-  unsigned char *buffer = &packet[3];
-  int c = 0;
-  int n;
+    unsigned char *buffer = &packet[3];
+    int c = 0;
+    int n;
 
-  n = size - 5;
+    n = size - 5;
 
-  while (n > 1) {
-    c+= (*(buffer)<<8) | *(buffer+1);
-    c = c & 0xffff;
-    n -= 2;
-    buffer += 2;
-  }
-  if (n>0) c = c^ (int)*(buffer++);
+    while (n > 1) {
+        c+= (*(buffer)<<8) | *(buffer+1);
+        c = c & 0xffff;
+        n -= 2;
+        buffer += 2;
+    }
+    if (n>0) c = c^ (int)*(buffer++);
 
-  return(c);
+    return(c);
 }
 
-int P2OSPacket::Receive( int fd )
+int P2OSPacket::Receive(int fd)
 {
-  unsigned char prefix[3];
-  //int skipped=0;
-  int cnt;
+    unsigned char prefix[3];
+    int cnt;
 
-  memset(packet,0,sizeof(packet));
+    memset(packet,0,sizeof(packet));
 
-  do
-  {
-    memset(prefix,0,sizeof(prefix));
-    //memset( prefix, 0, 3);
-
-    while(1)
+    do
     {
-      cnt = 0;
-      while( cnt!=1 )
-      {
-        if ( (cnt+=read( fd, &prefix[2], 1 )) < 0 )
+        memset(prefix,0,sizeof(prefix));
+
+        while(1)
         {
-          ROS_ERROR("Error reading packet header from robot connection: P2OSPacket():Receive():read():");
-          return(1);
+            cnt = 0;
+            while( cnt!=1 )
+            {
+                if ( (cnt+=read( fd, &prefix[2], 1 )) < 0 )
+                {
+                    ROS_ERROR("Error reading packet header from robot connection: P2OSPacket():Receive():read():");
+                    return(1);
+                }
+            }
+
+            if (prefix[0] == 0xFA && prefix[1] == 0xFB)
+                break;
+
+            timestamp = ros::Time::now();
+
+            //GlobalTime->GetTimeDouble(&timestamp);
+
+            prefix[0] = prefix[1];
+            prefix[1] = prefix[2];
+            //skipped++;
         }
-      }
+        //if (skipped>3) ROS_INFO("Skipped %d bytes\n", skipped);
 
-      if (prefix[0]==0xFA && prefix[1]==0xFB) break;
+        size = prefix[2]+3;
+        memcpy( packet, prefix, 3);
 
-      timestamp = ros::Time::now();
+        cnt = 0;
+        while( cnt!=prefix[2] )
+        {
+            if ((cnt+=read(fd, &packet[3+cnt],  prefix[2]-cnt)) < 0 )
+            {
+                ROS_ERROR("Error reading packet body from robot connection: P2OSPacket():Receive():read():");
+                return(1);
+            }
+        }
+    } while (!Check());
+    return(0);
+}
 
-      //GlobalTime->GetTimeDouble(&timestamp);
+int P2OSPacket::Build(unsigned char *data, unsigned char datasize) {
+    short chksum;
 
-      prefix[0]=prefix[1];
-      prefix[1]=prefix[2];
-      //skipped++;
-    }
-    //if (skipped>3) ROS_INFO("Skipped %d bytes\n", skipped);
+    size = datasize + 5;
 
-    size = prefix[2]+3;
-    memcpy( packet, prefix, 3);
+    /* header */
+    packet[0]=0xFA;
+    packet[1]=0xFB;
 
-    cnt = 0;
-    while( cnt!=prefix[2] )
-    {
-      if ( (cnt+=read( fd, &packet[3+cnt],  prefix[2]-cnt )) < 0 )
-      {
-        ROS_ERROR("Error reading packet body from robot connection: P2OSPacket():Receive():read():");
+    if ( size > 198 ) {
+        ROS_ERROR("Packet to P2OS can't be larger than 200 bytes");
         return(1);
-      }
     }
-  } while (!Check());
-  return(0);
+    packet[2] = datasize + 2;
+
+    memcpy(&packet[3], data, datasize);
+
+    chksum = CalcChkSum();
+    packet[3+datasize] = chksum >> 8;
+    packet[3+datasize+1] = chksum & 0xFF;
+
+    if (!Check()) {
+        ROS_ERROR("DAMN");
+        return(1);
+    }
+    return(0);
 }
 
-int P2OSPacket::Build( unsigned char *data, unsigned char datasize ) {
-  short chksum;
-
-  size = datasize + 5;
-
-  /* header */
-  packet[0]=0xFA;
-  packet[1]=0xFB;
-
-  if ( size > 198 ) {
-    ROS_ERROR("Packet to P2OS can't be larger than 200 bytes");
-    return(1);
-  }
-  packet[2] = datasize + 2;
-
-  memcpy( &packet[3], data, datasize );
-
-  chksum = CalcChkSum();
-  packet[3+datasize] = chksum >> 8;
-  packet[3+datasize+1] = chksum & 0xFF;
-
-  if (!Check()) {
-    ROS_ERROR("DAMN");
-    return(1);
-  }
-  return(0);
-}
-
-int P2OSPacket::Send( int fd)
+int P2OSPacket::Send(int fd)
 {
-  int cnt=0;
+    int cnt=0;
 
-  //ROS_INFO("Send(): ");
-  //PrintHex();
-  while(cnt!=size)
-  {
-    if((cnt += write( fd, packet, size )) < 0)
+    while (cnt != size)
     {
-      ROS_ERROR("Send");
-      return(1);
+        if((cnt += write( fd, packet, size )) < 0)
+        {
+            ROS_ERROR("Send");
+            return(1);
+        }
     }
-  }
-  return(0);
+    return(0);
 }
